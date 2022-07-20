d4ebf07606d4ec7480bb29a4b5737d580d9d388a
Paige Watson <paige@industriallogic.com>
2022-07-20 13:36:37 -0500

    initial workshop stuff

---
+++ src/main/java/dev/paigewatson/practice/Something.java
@@ -0,0 +1,5 @@
+package dev.paigewatson.practice;
+
+public class Something
+{
+}
---
+++ src/test/java/dev/paigewatson/practice/test.java
@@ -0,0 +1,14 @@
+package dev.paigewatson.practice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class test
+{
+    @Test
+    void should_do_something()
+    {
+        assertThat(true).isFalse();
+    }
+}
